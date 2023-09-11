c5c09f7179a965066b1ac7e8d3b928c7c5650a16
JinwooKim <leaprealm@gmail.com>
2023-09-11 14:37:02 +0900

    Day 1

---
+++ Server/GameServer/pch.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+#ifdef _DEBUG
+#pragma comment(lib, "ServerCore\\Debug\\ServerCore.lib")
+#pragma comment(lib, "Protobuf\\Debug\\libprotobufd.lib")
+#else
+#pragma comment(lib, "ServerCore\\Release\\ServerCore.lib")
+#pragma comment(lib, "Protobuf\\Release\\libprotobuf.lib")
+#endif
+
+#include "CorePch.h"
+
+#include "Protocol.pb.h"
+#include "Enum.pb.h"
+#include "Struct.pb.h"
+#include "ServerPacketHandler.h"
+
+using GameSessionRef = shared_ptr<class GameSession>;
